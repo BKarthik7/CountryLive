1ed148c79151a02e91430879c5a41a56364be307
B Karthik <bangikarthik7@gmail.com>
2025-01-17 10:50:25 +0530

    app

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './src/HomeScreen'
+import Splash from './src/Splash'
 
 const App = () => {
   const Stack = createNativeStackNavigator()
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Splash' screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Splash' component={Splash} />
         <Stack.Screen name='Home' component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
