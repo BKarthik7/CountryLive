7c1911a445aced9fbb00552867e239a401b0d6f4
B Karthik <bangikarthik7@gmail.com>
2025-01-17 10:49:37 +0530

    splash

---
+++ src/Splash.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import tw from '../lib/tailwind';
+import * as Animatable from 'react-native-animatable';
+import { FadeInDown } from 'react-native-reanimated';
+
+const Splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Home')
+    }, 3000);
+  }, [])
+  return (
+    <View style={tw`flex-1 justify-center items-center bg-redprimary`}>
+      <Animatable.Text 
+        style={tw`text-4xl text-white font-bold`} 
+        animation="fadeInDownBig"
+        duration={2000}
+      >
+          Country Live
+      </Animatable.Text>
+    </View>
+  )
+}
+
+export default Splash
+
+const styles = StyleSheet.create({})
