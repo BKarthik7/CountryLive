3610f94f7d735a217513d64d592b6ca097df98e3
B Karthik <bangikarthik7@gmail.com>
2025-01-18 23:07:53 +0530

    back action

--- src/NewsViewer.js
+++ src/NewsViewer.js
@@ -1,12 +1,24 @@
-import {StyleSheet} from 'react-native';
-import React from 'react';
-import {WebView} from 'react-native-webview';
+import React, { useEffect } from 'react';
+import { WebView } from 'react-native-webview';
+import { BackHandler } from 'react-native';
 
-const NewsViewer = ({navigation, route}) => {
-  const {url} = route.params;
-  return <WebView source={{uri: url}} style={{flex: 1}} />;
+const NewsViewer = ({ navigation, route }) => {
+  const { url } = route.params;
+
+  useEffect(() => {
+    const backAction = () => {
+      navigation.navigate('Home');
+      return true;
+    };
+
+    BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', backAction);
+    };
+  }, [navigation]);
+
+  return <WebView source={{ uri: url }} style={{ flex: 1 }} />;
 };
 
 export default NewsViewer;
-
-const styles = StyleSheet.create({});
